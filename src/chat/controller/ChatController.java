3006b2a096625b853014bf12f6ed3a59ff267ef5
kuojsean <kuo.j.sean@gmail.com>
2017-10-17 09:40:42 -0600

    Created starting methods runner and controller.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,6 +1,19 @@
 package chat.controller;
 
+import chat.view.*;
+import chat.model.*;
+
 public class ChatController
 {
-
+	private ChatDisplay display;
+	
+	public ChatController()
+	{
+		display = new ChatDisplay();
+	}
+	
+	public void start()
+	{
+		
+	}
 }
--- src/chat/controller/ChatRunner.java
+++ src/chat/controller/ChatRunner.java
@@ -2,5 +2,9 @@ package chat.controller;
 
 public class ChatRunner
 {
-
+	public static void main(String [] args)
+	{
+		ChatController app = new ChatController();
+		app.start();
+	}
 }
