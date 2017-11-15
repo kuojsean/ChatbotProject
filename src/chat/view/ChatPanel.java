a10da7ab1f03f47332c89e7b077f5f95836095fa
kuojsean <kuo.j.sean@gmail.com>
2017-11-15 09:04:40 -0700

    Created the JPanel code and setup for all of the components.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,45 @@
 package chat.view;
 
-public class ChatPanel
-{
+import javax.swing.*;
+import chat.controller.*;
+import java.awt.*;
 
+
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	private JButton responseButton;
+	private SpringLayout baseLayout;
+	private JTextArea chatTexts;
+	private JTextField userInput;
+	
+	public ChatPanel(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		responseButton = new JButton ("Say");
+		baseLayout = new SpringLayout();
+		chatTexts = new JTextArea();
+		userInput = new JTextField();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
 }
